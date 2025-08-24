b0078a7d927d333839bae72f415803149d20fdf2
Grensuke <grensuke@gmail.com>
2025-08-24 22:45:10 +0530

    Did some self learning fr

---
+++ Basics/primeFr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num;
+    cout << "Enter a number:";
+    cin >> num;
+    int x;
+    x > 0;
+    x != num;
+    x != 1;
+    
+
+
+    if ( (num % x ) != 0 ) {
+        cout << "The given number is a prime number.";
+    }
+
+    else {
+        cout << "The given number is a composite number.";
+    }
+}
